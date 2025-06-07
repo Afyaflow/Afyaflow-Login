60b221591cacec7e06306623c935fc652e4bb715
shitialo <karimshitialo@gmail.com>
2025-06-07 22:16:54 +0300

    docker-entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
-# Wait for database to be ready
-echo "Waiting for PostgreSQL..."
-while ! nc -z db 5432; do
-    sleep 0.1
-done
-echo "PostgreSQL started"
 
-# Make migrations for the users app first
-python manage.py makemigrations users
 
-# Apply migrations
-python manage.py migrate users
-python manage.py migrate
+echo "Applying database migrations..."
+python manage.py migrate --noinput
 
-# Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
-# Start server
-python manage.py runserver 0.0.0.0:8000 
+
+exec "$@" 
