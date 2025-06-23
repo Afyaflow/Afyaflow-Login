1add7ae04771f53331635e00e0ad2e92ec7d6922
shitialo <karimshitialo@gmail.com>
2025-06-23 17:36:34 +0300

    debugging

--- users/graphql/mutations/social_auth.py
+++ users/graphql/mutations/social_auth.py
@@ -7,12 +7,14 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.microsoft.views import MicrosoftGraphOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.helpers import complete_social_login
-from allauth.socialaccount.models import SocialLogin, SocialToken, SocialApp
+from allauth.socialaccount.models import SocialLogin, SocialToken, SocialApp, SocialAccount
+from allauth.account.models import EmailAddress
 import requests
 import logging
 
 from ..types import AuthPayloadType
 from ..services import create_auth_payload
+from users.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -62,42 +64,64 @@ class GoogleLoginMutation(BaseSocialAuthMutation):
             adapter = GoogleOAuth2Adapter(info.context)
             app = cls.get_social_app('google', info.context)
             
-            # Create token
-            token = SocialToken(
-                app=app,
-                token=access_token,
-            )
-            if id_token:
-                token.token_secret = id_token
-
             # Fetch user info from Google
             headers = {"Authorization": f"Bearer {access_token}"}
             response = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', headers=headers)
             if response.status_code != 200:
                 raise Exception("Failed to get user info from Google")
             
-            # Create a response object that the adapter expects
-            oauth2_response = OAuth2Response(response)
+            user_data = response.json()
+            
+            # Get or create user
+            email = user_data.get('email')
+            if not email:
+                raise Exception("Email not provided by Google")
 
-            # Get user info from Google
-            provider = adapter.get_provider()
-            user_info = adapter.complete_login(info.context, app, token, response=oauth2_response)
-            user_info.token = token
-
-            # Create social login instance
-            social_login = SocialLogin(
-                user=user_info.user,
-                account=user_info.account,
-                token=token
-            )
-
-            # Complete the social login process
             with transaction.atomic():
-                user = social_login.save(info.context)
-                
-                if not user.is_active:
-                    return cls(auth_payload=None, errors=["Your account is not active."])
-                
+                try:
+                    # Try to find existing user
+                    user = User.objects.get(email=email)
+                except User.DoesNotExist:
+                    # Create new user
+                    user = User.objects.create_user(
+                        email=email,
+                        first_name=user_data.get('given_name', ''),
+                        last_name=user_data.get('family_name', ''),
+                        is_active=True
+                    )
+                    user.set_unusable_password()
+                    user.save()
+
+                    # Create verified email
+                    EmailAddress.objects.create(
+                        user=user,
+                        email=email,
+                        primary=True,
+                        verified=True
+                    )
+
+                # Get or create social account
+                social_account, created = SocialAccount.objects.get_or_create(
+                    provider='google',
+                    uid=user_data.get('sub'),
+                    defaults={
+                        'user': user,
+                        'extra_data': user_data
+                    }
+                )
+
+                if not created:
+                    social_account.extra_data = user_data
+                    social_account.save()
+
+                # Create social token
+                social_token = SocialToken.objects.create(
+                    app=app,
+                    account=social_account,
+                    token=access_token,
+                    token_secret=id_token if id_token else ''
+                )
+
                 # Log the user in
                 login(info.context, user)
                 
@@ -118,42 +142,64 @@ class MicrosoftLoginMutation(BaseSocialAuthMutation):
             adapter = MicrosoftGraphOAuth2Adapter(info.context)
             app = cls.get_social_app('microsoft', info.context)
             
-            # Create token
-            token = SocialToken(
-                app=app,
-                token=access_token,
-            )
-            if id_token:
-                token.token_secret = id_token
-
             # Fetch user info from Microsoft Graph API
             headers = {"Authorization": f"Bearer {access_token}"}
             response = requests.get('https://graph.microsoft.com/v1.0/me', headers=headers)
             if response.status_code != 200:
                 raise Exception("Failed to get user info from Microsoft")
             
-            # Create a response object that the adapter expects
-            oauth2_response = OAuth2Response(response)
+            user_data = response.json()
+            
+            # Get or create user
+            email = user_data.get('mail') or user_data.get('userPrincipalName')
+            if not email:
+                raise Exception("Email not provided by Microsoft")
 
-            # Get user info from Microsoft
-            provider = adapter.get_provider()
-            user_info = adapter.complete_login(info.context, app, token, response=oauth2_response)
-            user_info.token = token
-
-            # Create social login instance
-            social_login = SocialLogin(
-                user=user_info.user,
-                account=user_info.account,
-                token=token
-            )
-
-            # Complete the social login process
             with transaction.atomic():
-                user = social_login.save(info.context)
-                
-                if not user.is_active:
-                    return cls(auth_payload=None, errors=["Your account is not active."])
-                
+                try:
+                    # Try to find existing user
+                    user = User.objects.get(email=email)
+                except User.DoesNotExist:
+                    # Create new user
+                    user = User.objects.create_user(
+                        email=email,
+                        first_name=user_data.get('givenName', ''),
+                        last_name=user_data.get('surname', ''),
+                        is_active=True
+                    )
+                    user.set_unusable_password()
+                    user.save()
+
+                    # Create verified email
+                    EmailAddress.objects.create(
+                        user=user,
+                        email=email,
+                        primary=True,
+                        verified=True
+                    )
+
+                # Get or create social account
+                social_account, created = SocialAccount.objects.get_or_create(
+                    provider='microsoft',
+                    uid=user_data.get('id'),
+                    defaults={
+                        'user': user,
+                        'extra_data': user_data
+                    }
+                )
+
+                if not created:
+                    social_account.extra_data = user_data
+                    social_account.save()
+
+                # Create social token
+                social_token = SocialToken.objects.create(
+                    app=app,
+                    account=social_account,
+                    token=access_token,
+                    token_secret=id_token if id_token else ''
+                )
+
                 # Log the user in
                 login(info.context, user)
                 
